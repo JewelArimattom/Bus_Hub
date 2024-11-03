6c326bad011fc46c2311a3920592e359f54dabe7
Jewel Thomas <jewelat50@gmail.com>
2024-11-03 13:14:20 +0530

    bus time

--- js/search.js
+++ js/search.js
@@ -2,59 +2,130 @@
 
 const busCollection = [
   { 
-      name: "Bus 101", 
+      name: "KSRTC", 
       schedule: [
-          { stop: "Downtown", time: "08:00 AM" },
-          { stop: "Midtown", time: "08:15 AM" },
-          { stop: "Uptown", time: "08:30 AM" }
+          { stop: "Pala", time: "06:15 AM" },
+          { stop: "Pravithanam", time: "06:30 AM" },
+          { stop: "Ullanad", time: "06:40 AM" },
+          { stop: "Kayyoor", time: "07:05 AM" }
       ] 
   },
   { 
-    name: "Bus 101", 
+    name: "ST JUDE", 
     schedule: [
-        { stop: "Downtown", time: "08:00 AM" },
-        { stop: "Midtown", time: "08:15 AM" },
-        { stop: "Uptown", time: "08:30 AM" }
+        { stop: "Plassnal", time: "06:15 AM" },
+        { stop: "Nariyanganam", time: "06:30 AM" },
+        { stop: "Pravithanam", time: "06:40 AM" },
+        { stop: "Pala", time: "07:00 AM" }
     ] 
 },
 { 
-    name: "Bus 101", 
+    name: "ST JUDE", 
     schedule: [
-        { stop: "Downtown", time: "08:00 AM" },
-        { stop: "Midtown", time: "08:15 AM" },
-        { stop: "Uptown", time: "08:30 AM" }
+        { stop: "Plassnal", time: "06:30 AM" },
+        { stop: "Nariyanganam", time: "06:45 AM" },
+        { stop: "Pravithanam", time: "07:05 AM" }
     ] 
 },
 { 
-    name: "Bus 101", 
+    name: "KSRTC", 
     schedule: [
-        { stop: "Downtown", time: "08:00 AM" },
-        { stop: "Midtown", time: "08:15 AM" },
-        { stop: "Uptown", time: "08:30 AM" }
+        { stop: "Kayyoor", time: "07:10 AM" },
+        { stop: "Ullanad", time: "07:25 AM" },
+        { stop: "Pravithanam", time: "07:35 AM" },
+        { stop: "Pala", time: "07:50 AM" },
+        { stop: "Kottayam", time: "09:00 AM" }
     ] 
 },
 { 
-    name: "Bus 101", 
+    name: "ST JUDE", 
     schedule: [
-        { stop: "Downtown", time: "08:00 AM" },
-        { stop: "Midtown", time: "08:15 AM" },
-        { stop: "Uptown", time: "08:30 AM" }
+        { stop: "Pala", time: "07:30 AM" },
+        { stop: "Pravithanam", time: "07:45 AM" },
+        { stop: "Nariyanganam", time: "08:00 AM" },
+        { stop: "Plassnal", time: "08:15 AM" },
+        { stop: "Erattupetta", time: "08:30 AM" },
     ] 
 },
+{ 
+    name: "Meenakshi", 
+    schedule: [
+        { stop: "Plassnal", time: "07:20 AM" },
+        { stop: "Nariyanganam", time: "07:40 AM" },
+        { stop: "Pravithanam", time: "07:55 AM" },
+        { stop: "Pala", time: "08:10 AM" },
+        { stop: "Cherpunkal", time: "08:40 AM" },
+        { stop: "Ponkunnam", time: "09:30 AM" }
+    ] 
+},
+{ 
+    name: "MARIA", 
+    schedule: [
+        { stop: "Kottayam", time: "07:30 AM" },
+        { stop: "Pala", time: "08:25 AM" },
+        { stop: "Pravithanam", time: "08:40 AM" },
+        { stop: "Nariyanganam", time: "08:50 AM" },
+        { stop: "Plassnal", time: "09:00 AM" },
+        { stop: "Collage Padi", time: "09:05 AM" },
+        { stop: "Erattupetta", time: "09:20 AM" }
+    ] 
+},
+{ 
+    name: "ST JUDE", 
+    schedule: [
+        { stop: "Pala", time: "08:45 AM" },
+        { stop: "Pravithanam", time: "09:10 AM" },
+        { stop: "Nariyanganam", time: "09:20 AM" },
+        { stop: "Plassnal", time: "09:25 AM" },
+        { stop: "Collage Padi", time: "09:30 AM" },
+        { stop: "Erattupetta", time: "09:40 AM" },
+    ] 
+},
+{ 
+    name: "ST JUDE", 
+    schedule: [
+        { stop: "Erattupetta", time: "08:45 AM" },
+        { stop: "Collage padi", time: "08:50 AM" },
+        { stop: "Panackapalam", time: "08:55 AM" },
+        { stop: "Plassnal", time: "09:00 AM" },
+        { stop: "Nariyanganam", time: "09:15 AM" },
+        { stop: "Pravithanam", time: "09:30 AM" },
+        { stop: "Pala", time: "09:45 AM" }
+    ] 
+},
+{ 
+    name: "MARIA", 
+    schedule: [
+        { stop: "Erattupetta", time: "09:35 AM" },
+        { stop: "Collage padi", time: "09:40 AM" },
+        { stop: "Panackapalam", time: "09:43 AM" },
+        { stop: "Plassnal", time: "09:45 AM" },
+        { stop: "Nariyanganam", time: "09:50 AM" },
+        { stop: "Pravithanam", time: "10:00 AM" },
+        { stop: "Pala", time: "10:15 AM" },
+        { stop: "Kottayam", time: "11:00 AM" },
+        
+    ] 
+},
+{ 
+    name: "KUZHITHOTTU", 
+    schedule: [
+        { stop: "Erattupetta", time: "08:15 AM" },
+        { stop: "Collage padi", time: "08:17 AM" },
+        { stop: "Panackapalam", time: "08:20 AM" },
+        { stop: "Plassnal", time: "08:25 AM" },
+        { stop: "Ullanad", time: "08:40 AM" },
+        { stop: "Pravithanam", time: "08:45 AM" },
+        { stop: "Pala", time: "09:00 AM" }
+    ] 
+},
+
   { 
-      name: "Bus 102", 
+      name: "EMIK", 
       schedule: [
-          { stop: "Eastside", time: "09:30 AM" },
-          { stop: "City Center", time: "09:45 AM" },
-          { stop: "Westside", time: "10:00 AM" }
-      ] 
-  },
-  { 
-      name: "Bus 103", 
-      schedule: [
-          { stop: "North End", time: "10:45 AM" },
-          { stop: "City Center", time: "11:00 AM" },
-          { stop: "South End", time: "11:15 AM" }
+          { stop: "Pala", time: "1:00 PM" },
+          { stop: "Thodupuzha", time: "02:00 PM" },
+          { stop: "Muvattupuzha", time: "03:00 PM" }
       ] 
   }
 ];
@@ -63,6 +134,41 @@ const busCollection = [
 
 /////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
+
+///auto suggestion
+
+const options = ["Pala", "Thodupuzha", "Muvattupuzha", "Pravithanam", "Erattupetta", "Cherpunkal", "Ponkunnam", "Collage Padi", "Panackapalam", "Ullanad", "Nariyanganam", "Kottayam","Plassnal",];
+
+function showSuggestions(inputId, suggestionsDivId) {
+    const input = document.getElementById(inputId);
+    const suggestionsDiv = document.getElementById(suggestionsDivId);
+    const query = input.value.toLowerCase();
+
+    // Clear any existing suggestions
+    suggestionsDiv.innerHTML = '';
+
+    if (query) {
+        // Filter options based on the input query
+        const filteredOptions = options.filter(option => option.toLowerCase().includes(query));
+        
+        // Create suggestion items
+        filteredOptions.forEach(option => {
+            const suggestionItem = document.createElement("div");
+            suggestionItem.classList.add("suggestion-item");
+            suggestionItem.textContent = option;
+
+            // Add click event to select suggestion
+            suggestionItem.onclick = () => {
+                input.value = option;
+                suggestionsDiv.innerHTML = ''; // Clear suggestions after selection
+            };
+
+            // Append suggestion to the suggestions div
+            suggestionsDiv.appendChild(suggestionItem);
+        });
+    }
+}
+
 
 
 
